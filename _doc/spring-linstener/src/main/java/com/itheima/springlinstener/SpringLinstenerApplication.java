401ce2c4dcc4995e3eb6d2f04905adb3eaa9bf79
yangsir <3300486066@qq.com>
2023-12-06 09:06:12 +0800

    23/12/06

---
+++ _doc/spring-linstener/src/main/java/com/itheima/springlinstener/SpringLinstenerApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.springlinstener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLinstenerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringLinstenerApplication.class, args);
+    }
+
+}
---
+++ _doc/spring-linstener/src/test/java/com/itheima/springlinstener/SpringLinstenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springlinstener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLinstenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
